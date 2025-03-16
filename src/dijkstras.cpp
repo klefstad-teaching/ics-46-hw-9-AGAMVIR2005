28352534439f368c725546f9ad79ac0a773a178a
Agamvir Mandair <mandaira@uci.edu>
2025-03-16 07:19:04 +0000

    lil mistake

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -35,7 +35,7 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 
 void print_path(const vector<int>& path, int total) {
     for (size_t i = 0; i < path.size(); ++i) {
-        cout << path[i] << (i < path.size() - 1 ? " -> " : "\n");
+        cout << path[i] << endl;
     }
     cout << "Total Weight: " << total << endl;
 }
